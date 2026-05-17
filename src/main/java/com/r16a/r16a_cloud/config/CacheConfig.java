21db0b94b3b2b330303312ad3ea73c822f4a311c
ramphy <ramphy_an@outlook.com>
2026-05-17 02:02:31 +0200

    min: redis config

--- src/main/java/com/r16a/r16a_cloud/config/CacheConfig.java
+++ src/main/java/com/r16a/r16a_cloud/config/CacheConfig.java
@@ -1,6 +1,11 @@
 package com.r16a.r16a_cloud.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -14,7 +19,9 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
-public class CacheConfig {
+public class CacheConfig implements CachingConfigurer {
+
+    private static final Logger log = LoggerFactory.getLogger(CacheConfig.class);
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
@@ -28,5 +35,30 @@ public class CacheConfig {
         return RedisCacheManager.builder(factory)
                 .withCacheConfiguration("thumbnails", thumbnailConfig)
                 .build();
+    }
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.warn("Cache GET error on '{}' key='{}': {}", cache.getName(), key, e.getMessage());
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.warn("Cache PUT error on '{}' key='{}': {}", cache.getName(), key, e.getMessage());
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+                log.warn("Cache EVICT error on '{}' key='{}': {}", cache.getName(), key, e.getMessage());
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.warn("Cache CLEAR error on '{}': {}", cache.getName(), e.getMessage());
+            }
+        };
     }
 }
